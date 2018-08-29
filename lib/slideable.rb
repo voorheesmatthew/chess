fb240bdf193036ad23a4122f4368ce8219b908ba
matthew voorhees <voorhees.matthew@gmail.com>
2018-08-29 12:43:52 -0700

    Fix HORIZONTAL and DIAGONAL constant variables in slideable module

--- lib/slideable.rb
+++ lib/slideable.rb
@@ -50,51 +50,11 @@ end
 module Slideable
 
   def horizontal_dirs
-    @board.rows.each_index do |x|
-      row.each_index do |y|
-        if x == self.pos.first && y != self.pos.last
-          HORIZONTAL_DIRS << [x,y]
-        elsif y == self.pos.last && x != self.pos.first
-          HORIZONTAL_DIRS << [x,y]
-        end
-      end
-    end
+
   end
 
   def diagonal_dirs
-    start = self.pos
-    x = start.first
-    y = start.last
 
-    until x == 0 || y == 0
-      p x
-      p y
-      DIAGONAL_DIRS << [x-=1, y-=1]
-    end
-    x = start.first
-    y = start.last
-
-    until x == 7 || y == 7
-      p x
-      p y
-      DIAGONAL_DIRS << [x+=1,y+=1]
-    end
-    x = start.first
-    y = start.last
-
-    until x == 0 || y == 7
-      p x
-      p y
-      DIAGONAL_DIRS << [x-=1, y+=1]
-    end
-    x = start.first
-    y = start.last
-
-    until x == 7 || y == 0
-      p x
-      p y
-      DIAGONAL_DIRS << [x+=1, y-=1]
-    end
   end
 
   def moves
@@ -103,11 +63,11 @@ module Slideable
 
   private
 
-  HORIZONTAL_DIRS = []
-  DIAGONAL_DIRS = []
+  HORIZONTAL_DIRS = [[0,1],[0,-1],[1,0],[-1,0]]
+  DIAGONAL_DIRS = [[1,1],[1,-1],[-1,-1],[-1,1]]
 
   def move_dirs
-    
+
   end
 
   def grow_unblocked_moves_in_dir(dx, dy)
